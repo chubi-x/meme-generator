43da531d04e536b2f6156f1af8528fbc4bd4c64a
chubi-x <chubixax@protonmail.com>
2022-08-03 09:45:00 +0100

    feat: style changes

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -9,12 +9,12 @@ export default function Form() {
           <FormInput placeholder="enter top text" />
           <FormInput placeholder="enter bottom text" />
         </div>
-        <div className="mx-auto w-full px-20">
+        <div className="mx-auto w-full md:px-20 px-16">
           <button className="w-full rounded p-5 bg-gradient-[40deg] from-purple1 via-blue to-purple2 text-white text-lg">
             Generate meme
           </button>
         </div>
-        <div className="mx-auto p-12 md:px-20">
+        <div className="mx-auto p-16 md:px-20">
           <img src={meme} alt="" className="max-w-auto max-h-auto" />
         </div>
       </div>
--- src/Components/FormInput.js
+++ src/Components/FormInput.js
@@ -2,7 +2,7 @@ export default function FormInput(props) {
   return (
     <div className="md:w-1/2 w-full md:p-10 p-6">
       <input
-        className="border-2 w-full h-12 rounded focus:outline-none text-center"
+        className="border-2 w-full h-12 rounded focus:outline-none text-center text-black"
         type="text"
         placeholder={props.placeholder}
       />
