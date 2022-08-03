7b8677ccae8dd527a7fe58f50f7f7b4ba6a3ddea
chubi-x <chubixax@protonmail.com>
2022-08-03 14:23:12 +0100

    chore: style changes

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -13,20 +13,20 @@ export default function Form() {
   return (
     <Fragment>
       <div className="container flex flex-col mx-auto md:px-32">
-        <div className="flex md:flex-row flex-col pt-20 p-10">
+        <div className="flex md:flex-row flex-col pt-10 p-10 pb-0">
           <FormInput placeholder="enter top text" />
           <FormInput placeholder="enter bottom text" />
         </div>
-        <div className="mx-auto w-full md:px-20 px-16">
+        <div className="mx-auto w-full md:px-20 px-16 btn">
           <button
             onClick={getMemes}
-            className="w-full rounded p-5 bg-gradient-[40deg] from-purple1 via-blue to-purple2 text-white text-lg"
+            className=" w-full rounded p-5 bg-gradient-[40deg] from-purple1 via-blue to-purple2 text-white text-lg"
           >
             Generate meme
           </button>
         </div>
-        <div className="mx-auto p-16 md:px-20">
-          <img src={imgUrl} alt="" className="max-w-auto max-h-auto" />
+        <div className="mx-auto p-16 md:px-20 pt-5">
+          <img src={imgUrl} alt="" className="max-w-full max-h-72" />
         </div>
       </div>
     </Fragment>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,8 +3,8 @@ import logo from "../images/logo.png";
 export default function Header() {
   return (
     <nav className="bg-gradient-[40deg] from-purple1 via-blue to-purple2">
-      <div className="w-100 mr-0 mt-0 container flex font-karla items-center">
-        <div className="flex space-x-5 p-2 pb-5 pl-20 items-center">
+      <div className="w-100 mr-0 mt-0 container flex font-karla items-center h-30">
+        <div className="flex space-x-5 p-2 pb-5 items-center md:pl-20 ">
           <img src={logo} alt="logo" className="w-20" />
           <span className="text-white text-4xl font-bold pt-5">
             Meme-o-matic{" "}
