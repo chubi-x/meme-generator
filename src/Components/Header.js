be4a7d2f0076b49c626beee8665ce7ce418769e4
chubi-x <chubixax@protonmail.com>
2022-08-05 16:11:36 +0100

    feat: change header bg on click

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,13 @@
 import React from "react";
 import logo from "../images/logo.png";
-export default function Header() {
+export default function Header(props) {
+  const leftGradient = props.gradient[0];
+  const middleGradient = props.gradient[1];
+  const rightGradient = props.gradient[2];
+
+  let navGradient = `bg-gradient-[40deg] ${leftGradient} ${middleGradient} ${rightGradient}`;
   return (
-    <nav className="bg-gradient-[40deg] from-purple1 via-blue to-purple2">
+    <nav className={navGradient}>
       <div className="w-100 mr-0 mt-0 container flex font-karla items-center h-30">
         <div className="flex space-x-5 p-2 pb-5 items-center md:pl-20 ">
           <img src={logo} alt="logo" className="w-20" />
